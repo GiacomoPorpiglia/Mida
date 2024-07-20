c6c1170c40161faefeb89f57188313447b5746c3
Giacomo Porpiglia <giacomo.porpiglia@gmail.com>
2024-07-20 14:49:45 +0200

    new LMR depth limit (~+10 elo)

--- src/search.cpp
+++ src/search.cpp
@@ -563,7 +563,7 @@ static inline int search(int depth, int alpha, int beta, SearchStack* ss) {
                     R += !improving; // increase reduction if we are not improving
                     R += is_quiet && !see(move, -50 * depth);   // we increase the reduction if the move is quiet, because they are less likely to be the best move
 
-                    R -= history / 8192;                    
+                    R -= history / 4000;                    
 
                     R -= 2 * isKillerMove; // if the move is a killer move, we want to search it deeper, therefore we make the reduction smaller
 
